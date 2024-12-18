bd9bb8aeffd9e655ac03095ba549edb93ab6271a
Timur <dimtimur7676@gmail.com>
2024-12-18 13:19:28 +0800

    add allure

--- test/test_login.py
+++ test/test_login.py
@@ -1,5 +1,10 @@
+import allure
+
+
+@allure.title('тест неудачного логина')
 def test_login(login_page):
     login_page.open_page()
-    login_page.login_user('hdfsdhaf@wqe.es', 'dbfsdnf')
+    with allure.step('вводим логин и пароль'):
+        login_page.login_user('hdfsdhaf@wqe.es', 'dbfsdnf')
     login_page.check_error_msg( 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
 
